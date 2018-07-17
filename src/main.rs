f062cee895f4f1710a10de501904e5d1711af197
matthieusb <sauboua.beneluz.matthieu@gmail.com>
2018-07-17 17:54:46 +0200

    test: adding some basic integration tests examples on cli

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,6 @@ extern crate build_tool_config;
 use build_tool_config::model::arguments::Arguments;
 
 fn main() {
-    let arguments = handle_arguments();
-    println!("{:?}", arguments);
-}
-
-fn handle_arguments() -> Arguments {
-    Arguments::from_args()
+    let arguments = Arguments::from_args();
+    println!("{:?}", arguments); // ! TODO To remove once features are correctly analyzed
 }
